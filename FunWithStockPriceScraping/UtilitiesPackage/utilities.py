5b92a600072d3706825fe46ee0d6563b9cb68744
syota <syota@DESKTOP-TAASRT1.wifi.uc.edu>
2022-10-20 08:32:23 -0400

    Added another stock

--- FunWithStockPriceScraping/UtilitiesPackage/utilities.py
+++ FunWithStockPriceScraping/UtilitiesPackage/utilities.py
@@ -49,6 +49,10 @@ def demo():
     market_cap = get_stock_current_market_cap("GOOGL")
     print("nicholdw: GOOGL Market Cap", market_cap)
     
+    price = get_stock_price("AAPL")
+    print("Yotassg: AAPL Price", price)
+    market_cap = get_stock_current_market_cap("AAPL")
+    print("Yotassg: AAPL Market Cap", market_cap)
     
 def buildPortfolio():
     # Build a Portfolio and scrape that
