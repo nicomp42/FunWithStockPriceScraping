baf6c9ba0f076c59816555c3485f895b067c1943
dburk <dburk@LAPTOP-1OJF02I0.wifi.uc.edu>
2022-10-20 08:13:00 -0400

    New Commit

--- FunWithStockPriceScraping/UtilitiesPackage/utilities.py
+++ FunWithStockPriceScraping/UtilitiesPackage/utilities.py
@@ -49,6 +49,12 @@ def demo():
     market_cap = get_stock_current_market_cap("GOOGL")
     print("nicholdw: GOOGL Market Cap", market_cap)
     
+    price = get_stock_price("AAPL")
+    print("burkhadj: AAPL Price", price)
+    market_cap = get_stock_current_market_cap("AAPL")
+    print("burkhadj: AAPL Market Cap", market_cap)
+    
+    
     
 def buildPortfolio():
     # Build a Portfolio and scrape that
