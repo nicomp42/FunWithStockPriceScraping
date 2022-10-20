116c06df480bb2bf70e5c5f88e434417dc46e544
myyna <myyna@DESKTOP-95NQD5D.wifi.uc.edu>
2022-10-20 08:13:20 -0400

    Add nguye2m6

--- FunWithStockPriceScraping/UtilitiesPackage/utilities.py
+++ FunWithStockPriceScraping/UtilitiesPackage/utilities.py
@@ -49,10 +49,14 @@ def demo():
     market_cap = get_stock_current_market_cap("GOOGL")
     print("nicholdw: GOOGL Market Cap", market_cap)
     
+    price = get_stock_price("ORCL")
+    print("nguye2m6: ORCL Price", price)
+    market_cap = get_stock_current_market_cap("ORCL")
+    print("nguye2m6: ORCL Market Cap", market_cap)
     
 def buildPortfolio():
     # Build a Portfolio and scrape that
-    myPortfolio = {"Google":"GOOGL", "Coke":"KO","Procter and Gamble":"PG","Home Depot":"HD"}
+    myPortfolio = {"Google":"GOOGL", "Coke":"KO","Procter and Gamble":"PG","Home Depot":"HD", "Oracle":"ORCL"}
 #    for i in range(0,100000000):
     for key in myPortfolio:
         print(myPortfolio[key] + "...")
